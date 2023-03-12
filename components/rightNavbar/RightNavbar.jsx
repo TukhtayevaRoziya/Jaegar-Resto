b40480192de154f278d5951e6f29c268ecd61664
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-03-12 23:13:12 +0500

    change

--- components/rightNavbar/RightNavbar.jsx
+++ components/rightNavbar/RightNavbar.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { AiOutlineDelete } from "react-icons/ai";
-import {IoWalletOutline} from 'react-icons/io5'
+import { IoWalletOutline } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
-import { Drawer } from "antd";
+import { Drawer, Select } from "antd";
 
 import i1 from "../../assets/card_i2.png";
 import { DELETE_ORDER, GET_ORDER } from "../redux/actions/types";
@@ -98,14 +98,60 @@ const RightNavbar = () => {
             <h2> $ {totalPrice}</h2>
           </div>
           <button onClick={showDrawer}>Continue to Payment</button>
-          <Drawer title="" placement="right" onClose={onClose} open={open} className={styles.drawer}>
+          <Drawer
+            title=""
+            placement="right"
+            onClose={onClose}
+            open={open}
+            className={styles.drawer}
+          >
             <h1>Payment</h1>
             <p>{data.length} payment method available</p>
             <h2>Payment Method</h2>
             <div className={styles.carts}>
               <div className={styles.carts__box}>
                 <IoWalletOutline />
+                <h1>Credit Card</h1>
               </div>
+              <div className={styles.carts__box}>
+                <IoWalletOutline />
+                <h1>Paypal</h1>
+              </div>
+              <div className={styles.carts__box}>
+                <IoWalletOutline />
+                <h1>Cash</h1>
+              </div>
+            </div>
+            <div className={styles.inp}>
+              <div>
+                <h1>Cardholder Name</h1>
+                <input placeholder="Levi Ackerman" />
+              </div>
+              <div>
+                <h1>Card Number</h1>
+                <input placeholder="2564 1421 0897 1244" type={"number"} />
+              </div>
+              <div className={styles.two_inp}>
+                <div>
+                  <h1>Expiration Date</h1>
+                  <input type={"date"} />
+                </div>
+                <div>
+                  <h1>CVV</h1>
+                  <input type={"date"} />
+                </div>
+              </div>
+            </div>
+            <div
+              styles={{ borderBottom: "none !important" }}
+              className={styles.inp}
+            >
+              <h1>Table no.</h1>
+              <input placeholder="140" type={"number"} />
+            </div>
+            <div className={styles.btn}>
+              <button onClick={onClose}>Cancel</button>
+              <button onClick={onClose}>Confirm Payment</button>
             </div>
           </Drawer>
         </div>
