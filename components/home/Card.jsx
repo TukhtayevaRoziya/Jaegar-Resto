f6ed8b138f6c6d9d90a9548592bca5e5d4acddb7
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-03-13 00:14:39 +0500

    fix

--- components/home/Card.jsx
+++ components/home/Card.jsx
@@ -4,24 +4,22 @@ import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
 
 import {createAction} from '../redux/actions/createAction'
-import styles from "./Home.module.css";
 import { CREATE_ORDER } from "../redux/actions/types";
 
-const Card = ({data1, data2, data3}) => {
-  const [data23, setData] = useState(data1);
+import styles from "./Home.module.css";
 
-  const { data } = useSelector((state)=>state.productReducer);
+const Card = ({data1, data2, data3}) => {
+  const [myData, setData] = useState(data1);
+
   const dispatch = useDispatch();
 
   if (!data1 || !data2 || !data3) {
     return <>Loading...</>;
   }
 
-  const dataMap = data23.map((d) =>{ 
+  const dataMap = myData.map((d) =>{ 
     const onClick = ()=>{
       dispatch(createAction('/', CREATE_ORDER, d))
-      console.log(d)
-      // dispatch(product(d))
     }
     return(
     <div key={d.id} onClick={onClick} className={styles.card_box}>
@@ -31,6 +29,7 @@ const Card = ({data1, data2, data3}) => {
       <h3>{d.available} available</h3>
     </div>
   )});
+  
   const handleChange = (value) => {
     const convertNum = Number(value);
     if (convertNum === 1) {
@@ -41,6 +40,7 @@ const Card = ({data1, data2, data3}) => {
       setData(data1);
     }
   };
+
   return (
     <div>
       <div className={styles.card_header}>
